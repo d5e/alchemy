55906c6779ab683e73426c1f0f92eb69b6326807
Henry Stenzel <d@hyperco.de>
2014-10-17 13:15:49 +0200

    minor fix removed validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,7 +4,7 @@ class Ingredient < ActiveRecord::Base
   belongs_to :substance
   belongs_to :dilution
   
-  validates :blend, :substance, :amount, presence: true
+  validates :substance, :amount, presence: true
   validates :amount, numericality: { greater_than: 0.0 }
 
 end
