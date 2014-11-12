0d092e7bf6334fca50cf0bcd375dbfe6236a5c4e
Henry Stenzel <d@hyperco.de>
2014-11-12 17:38:14 +0100

    minor DEV short blends by tags

--- app/helpers/blends_helper.rb
+++ app/helpers/blends_helper.rb
@@ -1,19 +1,17 @@
 module BlendsHelper
 
   # TODO use ElasticSearch here to get blends by tags count for performance increase
-  def blends_by_exclusive_tags
+  def blends_by_tags
     tag_counts = Hash.new(0)
     Blend.pluck(:sensory_tags).flatten.join(' ').gsub(/\W+/,' ').split(' ').each do |tag|
       tag_counts[tag] += 1
     end
     tcg = tag_counts.to_a.sort{ |a,b| a.last <=> b.last }
-    available = Blend.pluck(:id)
     ordered = {}
+    all_blends = Blend.all.to_a
     tcg.each do |tc|
-      bs = Blend.where(:id => available).where("sensory_tags LIKE '%#{tc.first}%'")
-      puts "##{tc.first}"
-      ordered[tc.first] = bs.to_a
-      available -= bs.map(&:id)
+      tag = tc.first.downcase
+      ordered[tag] = all_blends.select{|blend| blend.sensory_tags[/\b#{tag}\b/i] }.to_a
     end
     Rails.logger.info ordered.keys
     ordered.values.each do |vv|
