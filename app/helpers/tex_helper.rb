2ad9a36d60aa0b45455921a41325d03fd3c52d46
Henry Stenzel <d@hyperco.de>
2014-11-14 20:17:44 +0100

    introduced basic TeX escape

---
+++ app/helpers/tex_helper.rb
@@ -0,0 +1,20 @@
+module TexHelper
+  
+  REGULAR_SPECIAL_CHARACTERS = %w(# $ % ^ & _ { } ~ \\)
+  
+  # escape tex
+  def etex(stringy)
+    return stringy unless stringy
+    escaped = stringy.gsub /[#{Regexp.quote REGULAR_SPECIAL_CHARACTERS.join ''}]/ do |c|
+      if c == '\\'
+        '\textbackslash'
+      else
+        "\\#{c}"
+      end
+    end
+    escaped.html_safe
+  end
+
+  
+  
+end
