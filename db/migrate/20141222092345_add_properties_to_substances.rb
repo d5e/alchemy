14c0ba3776e50cb967555ec7ed971a8a4baaa9a4
Henry Stenzel <d@hyperco.de>
2014-12-22 13:20:37 +0100

    minor blends UI + migration ; TODO: ingredients submit highlighted

---
+++ db/migrate/20141222092345_add_properties_to_substances.rb
@@ -0,0 +1,8 @@
+class AddPropertiesToSubstances < ActiveRecord::Migration
+  def change
+    add_column :substances, :vp_mmHg_25C, :float
+    add_column :substances, :bpC_760mmHg, :float
+    add_column :substances, :tenacity_h, :integer, limit: 2
+    add_column :ingredients: :highlighted, :boolean
+  end
+end
