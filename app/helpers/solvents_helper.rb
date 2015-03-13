1bcd76f1938705737b048598ff617be2c3b1aeb7
Henry Stenzel <d@hyperco.de>
2015-03-13 15:06:45 +0100

    minor

--- app/helpers/solvents_helper.rb
+++ app/helpers/solvents_helper.rb
@@ -1,2 +1,18 @@
 module SolventsHelper
+  
+  def solvent_link(solvent_or_dilution)
+    if solvent_or_dilution.is_a?(Dilution)
+      solvent = solvent_or_dilution.solvent
+      dilution = solvent_or_dilution
+      if dilution.concentration == 0.0
+        text = dilution.solvent
+      else
+        text = "#{number_with_precision dilution.concentration * 100} % in #{dilution.solvent}"
+      end
+      link_to text, solvent_path(dilution.solvent, dilution_id: dilution.id) rescue "dilution_#{dilution.id}"
+    else
+      link_to solvent_or_dilution, solvent_or_dilution
+    end
+  end
+  
 end
